1e1b29d16237fd0d52b29bb4c843b4d1c8c08b61
Riceball LEE <snowyu.lee@gmail.com>
2022-08-19 18:46:45 +0800

    feat: add forceAdminRest and refactor genField

--- flow_config.lua
+++ flow_config.lua
@@ -1,38 +1,57 @@
-local minetest, quiz, yaml, flow, DIR_DELIM = minetest, quiz, yaml, flow, DIR_DELIM
+local minetest, quiz, flow = minetest, quiz, flow
 
-local formspec_escape = minetest.formspec_escape
+-- local formspec_escape = minetest.formspec_escape
 
 local S = quiz_ui.get_translator
-local qS = quiz.get_translator
 
 local settings = quiz.settings
 -- local Quizzes = quiz.quizzes
-local loadConfig = quiz.loadConfig
-local saveConfig = quiz.saveConfig
+-- local loadConfig = quiz.loadConfig
+-- local saveConfig = quiz.saveConfig
 -- GUI elements are accessible with flow.widgets. Using
 -- `local gui = flow.widgets` is recommended to reduce typing.
 local gui = flow.widgets
 local getSession = quiz.getSession
 
-local function on_config_number(name)
-  return function(player, ctx)
-    local v = tonumber(ctx.form[name])
-    if type(v) == "number" then
-      settings[name] = v
-    end
+local function set_num(form, name)
+  local v = tonumber(form[name])
+  if type(v) == "number" then
+    settings[name] = v
   end
 end
 
-local function genNumField(label, name)
-  return
-    -- gui.Label {label = label},
-    gui.Field{
-      label = label,
-      name= name,
-      w = 3.75,
-      default = "" .. settings[name],
-      on_event = on_config_number(name),
-    }
+local function set_bool(form, name)
+  local v = form[name]
+  if type(v) == "boolean" then
+    settings[name] = v
+  end
+end
+
+local function on_config(player, ctx)
+  set_num(ctx.form, "totalPlayTime")
+  set_num(ctx.form, "restTime")
+  set_num(ctx.form, "idleInterval")
+  set_num(ctx.form, "checkInterval")
+  set_bool(ctx.form, "forceAdminRest")
+end
+
+local function genField(label, name, type)
+  if type == nil then type = "Field" end
+  local params = {
+    label = label,
+    name= name,
+    -- on_event = on_config_number(name),
+    on_event = on_config,
+  }
+
+  if type == "Checkbox" then
+    params.select = settings[name]
+  else
+    params.default = "" .. settings[name]
+    params.w = 3.75
+  end
+
+  return gui[type](params)
 end
 
 local function getOnlinePlayerNames()
@@ -50,8 +69,8 @@ local function flowConfig(player, ctx)
     -- min_w = 12,
     -- min_h = 9,
     gui.HBox{
-      genNumField(S("Total Play Time").."("..S("minute") ..")", "totalPlayTime"),
-      genNumField(S("Rest Time").."("..S("minute") ..")", "restTime"),
+      genField(S("Total Play Time").."("..S("minute") ..")", "totalPlayTime"),
+      genField(S("Rest Time").."("..S("minute") ..")", "restTime"),
       gui.Dropdown {
           -- The value of this dropdown will be accessible from ctx.form.my_dropdown
           name = "onlinePlayers",
@@ -65,14 +84,15 @@ local function flowConfig(player, ctx)
               local selected_player = onlinePlayers[selected_idx]
               if selected_player then
                 quiz.setLastLeavedTime(selected_player, 0)
-                minetest.chat_send_player(player:get_player_name(), qS("reset @1 successful", selected_player))
+                minetest.chat_send_player(player:get_player_name(), S("reset @1 successful", selected_player))
               end
           end,
       },
     },
     gui.HBox{
-      genNumField(S("Idle Interval Time").."("..S("minute") ..")", "idleInterval"),
-      genNumField(S("Check Interval Time").."("..S("second") ..")", "checkInterval"),
+      genField(S("Idle Interval Time").."("..S("minute") ..")", "idleInterval"),
+      genField(S("Check Interval Time").."("..S("second") ..")", "checkInterval"),
+      genField(S("Force Admin Rest"), "forceAdminRest", "Checkbox"),
     },
   }
 end
